a036f0e30635239b45fca8aace64c2e9d8c0c38b
DuongDiesel <duongbgvip2.com>
2022-03-10 15:01:12 +0900

    removed os.path.dirname() on MEDIA_ROOT settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
 MEDIA_URL = "/media/"
 
 django_heroku.settings(locals())
