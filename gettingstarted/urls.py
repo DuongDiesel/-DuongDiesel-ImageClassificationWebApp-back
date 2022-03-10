87c944c5c249d1b79ac540cdf1340ad6805c3d38
DuongDiesel <duongbgvip2.com>
2022-03-10 16:36:37 +0900

    add api to urlpatterns

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -22,7 +22,8 @@ from django.urls import path, include
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('images.api.urls', namespace='api-images'))
 ]
 
 
