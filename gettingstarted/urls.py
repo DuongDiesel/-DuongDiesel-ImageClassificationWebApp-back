b61614c79c36ee55938c179eacf28b43b5c3c1dc
DuongDiesel <duongbgvip2.com>
2022-03-10 16:30:10 +0900

    api-auth  to models.py

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -9,6 +9,8 @@ import hello.views
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.urls import path, include
+
 # To add a new path, first import the app:
 # import blog
 #
@@ -19,8 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
 
