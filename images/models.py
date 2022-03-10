87578c95cb32c4ea468b1f5abe2b358d44670ffa
DuongDiesel <duongbgvip2.com>
2022-03-10 14:47:17 +0900

    change models and admin.py before migrate db

--- images/models.py
+++ images/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, decode_predictions, preprocess_input
 
 # Create your models here.
+class Image(models.Model):
+    picture = models.ImageField()
+    classified = models.CharField(max_length=200, blank=True)
+    uploaded = models.DateTimeField(auto_now_add=True)
