6d28609bf14fc08970c5293aab945101916ec1f5
DuongDiesel <duongbgvip2.com>
2022-03-10 15:21:33 +0900

    add __str__ to models.py

--- images/models.py
+++ images/models.py
@@ -8,3 +8,6 @@ class Image(models.Model):
     picture = models.ImageField()
     classified = models.CharField(max_length=200, blank=True)
     uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Image classfied at {}".format(self.uploaded.strftime('%Y-%m-%d %H:%M'))
