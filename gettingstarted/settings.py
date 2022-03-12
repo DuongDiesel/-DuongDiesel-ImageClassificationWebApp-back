ee7882119d719fe4f6bfba242b04004dbc99c8c9
DuongDiesel <duongbgvip2.com>
2022-03-12 09:07:42 +0900

    add cors

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -55,6 +55,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://serene-dusk-18231.herokuapp.com/"
 ]
 
 ROOT_URLCONF = "gettingstarted.urls"
