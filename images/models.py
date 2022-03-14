ae2926578224d26f38ff114e585c5f183c55afa5
DuongDiesel <duongbgvip2.com>
2022-03-14 15:41:57 +0900

    add print for test

--- images/models.py
+++ images/models.py
@@ -70,13 +70,14 @@ class Image(models.Model):
 
             ##########################
             file_model = os.path.join(settings.BASE_DIR, 'malaria_augmented_model.h5')
-            print("path loaded")
+            print(f'file_model as {file_model}')
             graph = ops.get_default_graph()
  
             with graph.as_default():
                 model = load_model(file_model)
                 pred = LABELS[model.predict_classes(to_pred)[0]]
                 self.classified = str(pred)
+                print("graph loaded")
                 print(f'classified as {pred}')
         except:
             print('failed to classify')
