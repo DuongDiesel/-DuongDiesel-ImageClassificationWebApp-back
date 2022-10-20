126cceaae0ae4039b82f708014f6cdf0c68f562a
DuongDiesel <duongbgvip2.com>
2022-10-21 00:23:07 +0900

    fix download models

--- images/models.py
+++ images/models.py
@@ -21,6 +21,8 @@ except:
 from torch import Tensor
 from torch.jit.annotations import List
 
+
+
 #---------------------------------------------------------------------------------------
 __all__ = ['DenseNet', 'densenet121', 'densenet169', 'densenet201', 'densenet161']
 
