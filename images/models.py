c21a634e722f30bc2c9411ac7a61e29e9a0a2a45
DuongDiesel <duongbgvip2.com>
2022-10-21 18:00:56 +0900

    fix process img

--- images/models.py
+++ images/models.py
@@ -353,7 +353,7 @@ class Image(models.Model):
             #img = load_img(self.picture, target_size=(224,224), color_mode='rgb')
             print('check 1')
             print(self.picture)
-            img = Image.open(self.picture)
+            img = Image.open(str(self.picture))
             print('check 2')
             print(img.shape)
             img = img.convert("L").convert("RGB")
